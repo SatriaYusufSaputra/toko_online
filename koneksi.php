947817b0c85d823ae6918d1523a617e42fcb99ce
Satria Yusuf Saputra <satriasys2@gmail.com>
2026-01-05 13:48:01 +0700

    fix bug

---
+++ koneksi.php
@@ -0,0 +1,9 @@
+<?php
+  $con = mysqli_connect("localhost", "root", "", "toko_online");
+  
+  //check connection
+  if(mysqli_connect_errno()){
+    echo "Failed to connect to MYSQL: " . mysqli_connect_error();
+    exit();
+  }
+?>
